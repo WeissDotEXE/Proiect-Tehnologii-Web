172f19dc0633f3bfc7d2d1b2b47b461c0eb36d7f
Mihnea Cristian Tache <tmihnea555@gmail.com>
2022-12-21 11:10:48 +0200

    router for study groups

---
+++ backend/routes/studyGroupsRouter.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+import {
+  getAllStudyGroups,
+  getStudyGroup,
+  createStudyGroup,
+  updateStudyGroup,
+  deleteStudyGroup,
+} from "../controllers/studyGroupController.js";
+
+const router = express.Router();
+
+router.route("/").get(getAllStudyGroups).post(createStudyGroup);
+router
+  .route("/:id")
+  .get(getStudyGroup)
+  .patch(updateStudyGroup)
+  .delete(deleteStudyGroup);
