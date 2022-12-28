d1afb1be14e5ab5f2f11d7c49887d8863e28d7c9
Mihnea Cristian Tache <tmihnea555@gmail.com>
2022-12-28 12:23:27 +0200

    study groups routes update

--- utils/queryes.js
+++ utils/queryes.js
@@ -1,12 +1,13 @@
 //STUDY GROUPS queries
 const createStudyGroupTable =
   "create table studygroups(id integer primary key,name,message,description)";
-const insertStudyGroup = `insert into studygroups(name,message,description) values(?,?,?)`;
-const insertMessageStudyGroup = `insert into studygroups(message) where id=? values(?,?)`;
-const selectAllStudyGroups = `Select * from studygroups`;
+const insertStudyGroup = `insert into studygroups(name,description) values(?,?,?)`;
+const insertMessageStudyGroup = `insert into studygroups(?) where id=? values(?,?)`;
+const selectAllStudyGroups = `Select * from studygroups where id=? values(?)`;
 const selectStudyGroup = `select * from studygroup where id=? values(?)`;
 const updateStudyGroup = `update studygroups set name=? where id=?`;
 const deleteStudyGroupTable = "drop table studygroups";
+const deleteStudyGroupQuery = "DELETE FROM studygroups WHERE id=? values(?)";
 
 //LOGIN/REGISTER queries]
 const insertUserQuery = "insert into users(name,email)  values(?,?,?)";
@@ -19,4 +20,6 @@ export {
   selectStudyGroup,
   updateStudyGroup,
   deleteStudyGroupTable,
+  deleteStudyGroupQuery,
+  insertUserQuery,
 };
