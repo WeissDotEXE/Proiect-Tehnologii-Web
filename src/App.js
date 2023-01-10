17ee861ae06800d5b089f41dc6964313fb2ed56b
Mihnea Cristian Tache <tmihnea555@gmail.com>
2023-01-10 17:41:57 +0200

    routing and pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import { Routes, Route } from "react-router-dom";
+import StudyGroupPage from "./pages/StudyGroupPage/StudyGroupPage";
+import AuthPage from "./pages/AuthPage/AuthPage";
+import TaskPage from "./pages/TaskPage/TaskPage";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      {/* <Header /> */}
+      <div>
+        <Routes>
+          <Route path="/studygroup" element={<StudyGroupPage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/taskpage" element={<TaskPage />} />
+          {/* <Route path="*" element={<Home />} /> */}
+        </Routes>
+      </div>
+      {/* <Footer /> */}
+    </div>
+  );
 }
 
 export default App;
