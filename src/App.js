5b887b9108371f0058111e14ec932f5a37712731
unknown <tudoroiuedurad20@stud.ase.ro>
2023-01-12 14:39:23 +0200

    rutele front

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import "./App.css";
 import LoginForm from "./components/Loginform";
 import Navbar from "./components/Navbar";
+
 function App() {
-  return <div></div>;
+  const location = useLocation();
+  return (
+    <div>
+      {location.pathname !== "/login" && <Navbar />}
+      <Routes>
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+      </Routes>
+    </div>
+  );
 }
 export default App;
